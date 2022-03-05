7acb59b9dbc299e2f2132f57ef2e80bbdfdd740b
Akanksha Singh Pradhan <akanksha_singh15@yahoo.com>
2022-03-05 19:35:36 +0100

    first commit

--- js/index.js
+++ js/index.js
@@ -4,12 +4,36 @@ const img = new Image();
 img.src = ('./images/road.png')
 const carImg = new Image();
  carImg.src = ('./images/car.png')
- let carCurrentPost = 185
- let obstacleCurrentPost = 80
+ let carCurrentPost = 185 
+ 
+class Obstacle {
+  constructor() {
+    this.x = this.get_x()
+    this.y = 100
+    this.height = 15
+    this.width = this.x * 0.4
+  }
+  get_x() {
+    let value = 0
+    while (value < 100 || value > 250) {
+      value = Math.random() * canvas.width
+    }
+    return value
+  }
+  update(num) {
+    this.y -= num;
+    context.fillStyle = "red";
+    context.fillRect(this.x, this.y, this.width, this.height)
+  }
+  reset() {
+    this.x = this.get_x()
+    this.y = 100
+    this.width = this.x * 0.4
+  }
+}
 
- 
- 
- 
+let obstacles = [new Obstacle()];
+
  window.onload = () => {
    document.getElementById('start-button').onclick = () => {
      startGame();
@@ -18,6 +42,7 @@ const carImg = new Image();
     function startGame() {
       context.drawImage(img , 70, 40);
       context.drawImage(carImg , 185, 200, 55, 90);
+      setInterval(moveObstacle, 100)
   }
 
   document.addEventListener('keydown', function (event){
@@ -37,27 +62,23 @@ const carImg = new Image();
     context.clearRect(0, 0, 700, 500)
     context.drawImage(img , 70, 40);
     context.drawImage(carImg , carCurrentPost, 200, 55, 90)
-    createObstacles(0)
-  }
-
-  function createObstacles(num) {
-    obstacleCurrentPost -= num;
-    context.fillStyle = "red";
-    context.fillRect(100, obstacleCurrentPost, 50, 15)
   }
 
   function moveObstacle() {
     context.clearRect(0, 0, 700, 500)
     context.drawImage(img , 70, 40);
     context.drawImage(carImg , carCurrentPost, 200, 55, 90);
-    if (obstacleCurrentPost < 400) {
-      createObstacles(-5)
-      console.log(obstacleCurrentPost) 
+
+    obstacles.forEach((obstacle) => {
+      if (obstacle.y < 450) {
+        obstacle.update(-5)
+      } else {
+        obstacle.reset()
+      }
+    })
+    if (obstacles.length === 1 && obstacles[0].y === 300) {
+        obstacles.push(new Obstacle())
     }
     
-    
   }
-  setInterval(moveObstacle, 100)
-
-   
 }
